b753c8e9e99c358bc113f8e0a77e57d606d44644
nicolasblumenroehr <86111342+nicolasblumenroehr@users.noreply.github.com>
2022-03-09 09:49:18 +0100

    Create __init__.py

---
+++ dicomSchemaMapping/__init__.py
@@ -0,0 +1,4 @@
+from dicomSchemaMapping.fillSchema import fillSchema
+from dicomSchemaMapping.readSchema import readSchema
+from dicomSchemaMapping.pyDicom import pyDicom
+from dicomSchemaMapping.initialize import initialize
